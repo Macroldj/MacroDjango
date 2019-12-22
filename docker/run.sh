47cb238263d572c4c4d07cb5d6c4c5642fca3d69
Alan <dejinx@qq.com>
2019-12-22 19:48:29 +0800

    add message

---
+++ docker/run.sh
@@ -0,0 +1,23 @@
+# 参数说明
+
+# run 0->1
+# start  docker start
+# stop   docker stop
+# spause docker contater spause
+# remove remove file, images, docker contater
+
+
+echo "boot all tornado projects"
+
+# 启动单元测试
+echo "boot unitTest of tornado"
+
+# 上传 docker hub
+echo "push docker image to docker hub"
+
+#docker push macroldj/macro_django:v1
+
+# build image
+echo "rm docker image tornado && build docker image"
+
+# inti
