431a17be4ea89d425ea58734fbfb4a2f562c4261
macroldj <macroldj@163.com>
2019-12-29 08:13:01 +0800

    create run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-docker image rm python:v1 
-docker build . -t python:v1
+# git pull code
+echo "update github code"
+git fetch --all
+git reset --hard origin/master
+git pull
 
+#
+echo "running services"
+docker-compose build
+docker-compose down
+docker-compose up -d
