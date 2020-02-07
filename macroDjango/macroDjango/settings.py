43ccccb96f5c38eb438debf0125fd59735caea2e
lidepeng <3343928047@qq.com>
2020-02-07 19:24:28 +0800

    add init

--- macroDjango/macroDjango/settings.py
+++ macroDjango/macroDjango/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'macroDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
-        'NAME': 'xiaoxiao',         # 你要存储数据的库名，事先要创建之
-        'USER': 'root',         # 数据库用户名
-        'PASSWORD': 'xiaoxiao',     # 密码
-        'HOST': '192.168.31.102',    # 主机
-        'PORT': '3306',         # 数据库使用的端口
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
+#         'NAME': 'xiaoxiao',         # 你要存储数据的库名，事先要创建之
+#         'USER': 'root',         # 数据库用户名
+#         'PASSWORD': 'xiaoxiao',     # 密码
+#         'HOST': '192.168.31.102',    # 主机
+#         'PORT': '3306',         # 数据库使用的端口
+#     }
+# }
 
 # cache
 
